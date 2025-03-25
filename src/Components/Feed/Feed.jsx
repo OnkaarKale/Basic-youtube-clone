75479bd93348c29e042a2a515a5e48136ee14a45
OnkaarKale <onkaarkale@gmail.com>
2025-03-25 18:16:46 +0530

    Initial commit

---
+++ src/Components/Feed/Feed.jsx
@@ -0,0 +1,187 @@
+import React from 'react'
+import './Feed.css'
+import thumbnail1 from '../../assets/thumbnail1.png'
+import thumbnail2 from '../../assets/thumbnail2.png'
+import thumbnail3 from '../../assets/thumbnail3.png'
+import thumbnail4 from '../../assets/thumbnail4.png'
+import thumbnail5 from '../../assets/thumbnail5.png'
+import thumbnail6 from '../../assets/thumbnail6.png'
+import thumbnail7 from '../../assets/thumbnail7.png'
+import thumbnail8 from '../../assets/thumbnail8.png'
+import { Links, NavLink } from 'react-router-dom'
+const Feed = () => {
+  return (
+<div className='feed'>
+ <NavLink to={`video/20/45121`} className='card'>
+        <img src={thumbnail1} alt="" />
+        <h2>Onkar kale is the best web developwer in the world</h2>
+        <h3>Mahabharat</h3>
+        <p>15k views bull; 2 days ago </p>
+        
+    </NavLink>
+    <div className='card'>
+        <img src={thumbnail2} alt="" />
+        <h2>Onkar kale is the best web developwer in the world</h2>
+        <h3>Mahabharat</h3>
+        <p>15k views bull; 2 days ago </p>
+    </div>
+    
+    <div className='card'>
+        <img src={thumbnail3} alt="" />
+        <h2>Onkar kale is the best web developwer in the world</h2>
+        <h3>Mahabharat</h3>
+        <p>15k views bull; 2 days ago </p>
+    </div>
+    
+    <div className='card'>
+        <img src={thumbnail4} alt="" />
+        <h2>Onkar kale is the best web developwer in the world</h2>
+        <h3>Mahabharat</h3>
+        <p>15k views bull; 2 days ago </p>
+    </div>
+    
+    <div className='card'>
+        <img src={thumbnail5} alt="" />
+        <h2>Onkar kale is the best web developwer in the world</h2>
+        <h3>Mahabharat</h3>
+        <p>15k views bull; 2 days ago </p>
+    </div>
+    
+    <div className='card'>
+        <img src={thumbnail6} alt="" />
+        <h2>Onkar kale is the best web developwer in the world</h2>
+        <h3>Mahabharat</h3>
+        <p>15k views bull; 2 days ago </p>
+    </div>
+    
+    <div className='card'>
+        <img src={thumbnail7} alt="" />
+        <h2>Onkar kale is the best web developwer in the world</h2>
+        <h3>Mahabharat</h3>
+        <p>15k views bull; 2 days ago </p>
+    </div>
+    
+    <div className='card'>
+        <img src={thumbnail8} alt="" />
+        <h2>Onkar kale is the best web developwer in the world</h2>
+        <h3>Mahabharat</h3>
+        <p>15k views bull; 2 days ago </p>
+    </div>
+    
+    <div className='card'>
+        <img src={thumbnail1} alt="" />
+        <h2>Onkar kale is the best web developwer in the world</h2>
+        <h3>Mahabharat</h3>
+        <p>15k views bull; 2 days ago </p>
+    </div>
+
+    <div className='card'>
+        <img src={thumbnail2} alt="" />
+        <h2>Onkar kale is the best web developwer in the world</h2>
+        <h3>Mahabharat</h3>
+        <p>15k views bull; 2 days ago </p>
+    </div>
+    
+    <div className='card'>
+        <img src={thumbnail3} alt="" />
+        <h2>Onkar kale is the best web developwer in the world</h2>
+        <h3>Mahabharat</h3>
+        <p>15k views bull; 2 days ago </p>
+    </div>
+    
+    <div className='card'>
+        <img src={thumbnail4} alt="" />
+        <h2>Onkar kale is the best web developwer in the world</h2>
+        <h3>Mahabharat</h3>
+        <p>15k views bull; 2 days ago </p>
+    </div>
+    
+    <div className='card'>
+        <img src={thumbnail5} alt="" />
+        <h2>Onkar kale is the best web developwer in the world</h2>
+        <h3>Mahabharat</h3>
+        <p>15k views bull; 2 days ago </p>
+    </div>
+    
+    <div className='card'>
+        <img src={thumbnail6} alt="" />
+        <h2>Onkar kale is the best web developwer in the world</h2>
+        <h3>Mahabharat</h3>
+        <p>15k views bull; 2 days ago </p>
+    </div>
+    
+    <div className='card'>
+        <img src={thumbnail7} alt="" />
+        <h2>Onkar kale is the best web developwer in the world</h2>
+        <h3>Mahabharat</h3>
+        <p>15k views bull; 2 days ago </p>
+    </div>
+    
+    <div className='card'>
+        <img src={thumbnail8} alt="" />
+        <h2>Onkar kale is the best web developwer in the world</h2>
+        <h3>Mahabharat</h3>
+        <p>15k views bull; 2 days ago </p>
+    </div>
+    
+    <div className='card'>
+        <img src={thumbnail1} alt="" />
+        <h2>Onkar kale is the best web developwer in the world</h2>
+        <h3>Mahabharat</h3>
+        <p>15k views bull; 2 days ago </p>
+    </div>
+
+    <div className='card'>
+        <img src={thumbnail2} alt="" />
+        <h2>Onkar kale is the best web developwer in the world</h2>
+        <h3>Mahabharat</h3>
+        <p>15k views bull; 2 days ago </p>
+    </div>
+    
+    <div className='card'>
+        <img src={thumbnail3} alt="" />
+        <h2>Onkar kale is the best web developwer in the world</h2>
+        <h3>Mahabharat</h3>
+        <p>15k views bull; 2 days ago </p>
+    </div>
+    
+    <div className='card'>
+        <img src={thumbnail4} alt="" />
+        <h2>Onkar kale is the best web developwer in the world</h2>
+        <h3>Mahabharat</h3>
+        <p>15k views bull; 2 days ago </p>
+    </div>
+    
+    <div className='card'>
+        <img src={thumbnail5} alt="" />
+        <h2>Onkar kale is the best web developwer in the world</h2>
+        <h3>Mahabharat</h3>
+        <p>15k views bull; 2 days ago </p>
+    </div>
+    
+    <div className='card'>
+        <img src={thumbnail6} alt="" />
+        <h2>Onkar kale is the best web developwer in the world</h2>
+        <h3>Mahabharat</h3>
+        <p>15k views bull; 2 days ago </p>
+    </div>
+    
+    <div className='card'>
+        <img src={thumbnail7} alt="" />
+        <h2>Onkar kale is the best web developwer in the world</h2>
+        <h3>Mahabharat</h3>
+        <p>15k views bull; 2 days ago </p>
+    </div>
+    
+    <div className='card'>
+        <img src={thumbnail8} alt="" />
+        <h2>Onkar kale is the best web developwer in the world</h2>
+        <h3>Mahabharat</h3>
+        <p>15k views bull; 2 days ago </p>
+    </div>
+    
+</div>
+  )
+}
+
+export default Feed
